bd4688d9f6212100e659315bf358151b5a68f99f
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-02-13 20:18:46 +0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="site">
+      <header>
+        <img src={logo} className="logo" alt="logo"/>
+        <a href="https://www.lifeatspotify.com/jobs">All Jobs</a>
       </header>
     </div>
   );
