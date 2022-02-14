d9fb20838959bec133911fa7d1a1aa8032bd0ff0
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-02-14 16:42:18 +0500

    add search total view

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,16 @@ function App() {
         <img src={logo} className="logo" alt="logo"/>
         <a href="https://www.lifeatspotify.com/jobs">All Jobs</a>
       </header>
-      <div className="container">
+      <div className="container search-row">
         <input type="text" value="Wait, where's your coffee?" />
         <div className="filters">
           <Selector label="Location"/>
           <Selector label="Category"/>
           <Selector label="Job type"/>
         </div>
+      </div>
+      <div className="container search-total">
+        <p><b>614</b> jobs in <b>all locations</b> in <b>all categories</b> in <b>all job types</b></p>
       </div>
     </div>
   );
